248ccf51232c1aa34fd91e082d1c93a24e239df3
Albbert123 <128749752+Albbert123@users.noreply.github.com>
2023-07-14 13:22:28 +0200

    Add files via upload

---
+++ Bloc2/s4/ej5/ui_MyForm.h
@@ -0,0 +1,129 @@
+/********************************************************************************
+** Form generated from reading UI file 'MyForm.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MYFORM_H
+#define UI_MYFORM_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDial>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+#include "MyGLWidget.h"
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MyForm
+{
+public:
+    QHBoxLayout *horizontalLayout;
+    MyGLWidget *widget;
+    QVBoxLayout *verticalLayout;
+    QLabel *label;
+    QDial *dial;
+    QSpacerItem *verticalSpacer_2;
+    QLabel *label_2;
+    QDial *dial_2;
+    QSpacerItem *verticalSpacer;
+    QPushButton *pushButton;
+
+    void setupUi(QWidget *MyForm)
+    {
+        if (MyForm->objectName().isEmpty())
+            MyForm->setObjectName(QString::fromUtf8("MyForm"));
+        MyForm->resize(819, 726);
+        horizontalLayout = new QHBoxLayout(MyForm);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        widget = new MyGLWidget(MyForm);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(widget->sizePolicy().hasHeightForWidth());
+        widget->setSizePolicy(sizePolicy);
+        widget->setMinimumSize(QSize(50, 50));
+
+        horizontalLayout->addWidget(widget);
+
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        label = new QLabel(MyForm);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        verticalLayout->addWidget(label);
+
+        dial = new QDial(MyForm);
+        dial->setObjectName(QString::fromUtf8("dial"));
+        dial->setMaximum(628);
+
+        verticalLayout->addWidget(dial);
+
+        verticalSpacer_2 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Minimum);
+
+        verticalLayout->addItem(verticalSpacer_2);
+
+        label_2 = new QLabel(MyForm);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        verticalLayout->addWidget(label_2);
+
+        dial_2 = new QDial(MyForm);
+        dial_2->setObjectName(QString::fromUtf8("dial_2"));
+        dial_2->setMaximum(628);
+
+        verticalLayout->addWidget(dial_2);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
+        pushButton = new QPushButton(MyForm);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        QSizePolicy sizePolicy1(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(pushButton->sizePolicy().hasHeightForWidth());
+        pushButton->setSizePolicy(sizePolicy1);
+
+        verticalLayout->addWidget(pushButton);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+
+        retranslateUi(MyForm);
+        QObject::connect(pushButton, SIGNAL(clicked()), MyForm, SLOT(close()));
+        QObject::connect(dial, SIGNAL(valueChanged(int)), widget, SLOT(changePsi(int)));
+        QObject::connect(dial_2, SIGNAL(valueChanged(int)), widget, SLOT(changeTheta(int)));
+        QObject::connect(widget, SIGNAL(setPsi(int)), dial, SLOT(setValue(int)));
+        QObject::connect(widget, SIGNAL(setTheta(int)), dial_2, SLOT(setValue(int)));
+
+        QMetaObject::connectSlotsByName(MyForm);
+    } // setupUi
+
+    void retranslateUi(QWidget *MyForm)
+    {
+        MyForm->setWindowTitle(QCoreApplication::translate("MyForm", "IDI-Lab", nullptr));
+        label->setText(QCoreApplication::translate("MyForm", "psi", nullptr));
+        label_2->setText(QCoreApplication::translate("MyForm", "theta", nullptr));
+        pushButton->setText(QCoreApplication::translate("MyForm", "&Sortir", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MyForm: public Ui_MyForm {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MYFORM_H
